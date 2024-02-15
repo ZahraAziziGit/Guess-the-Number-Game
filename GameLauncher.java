df1ebe4f0df22c9db4b30cfd741463d6364de143
Orion D <oriondeuniverse@gmail.com>
2024-02-15 10:37:39 +0330

    GameLaunvher added

---
+++ GameLauncher.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GameLauncher {
+    public static void main(String[] args){
+        GuessGame game = new GuessGame();
+        game.startGame();
+        }
+    }
+
